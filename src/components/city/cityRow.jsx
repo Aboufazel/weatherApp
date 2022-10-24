201a4566faf3dda0dfc997425f53d3a5cff02965
Aboufazel <a.ab2521@gmail.com>
2022-10-24 22:39:24 +0330

    city row edit

--- src/components/city/cityRow.jsx
+++ src/components/city/cityRow.jsx
@@ -4,16 +4,16 @@ import Container from "../container/Container";
 import {city} from "../../getapi/Database";
 import {Link} from "react-router-dom";
 const CityRow = () => {
-  return(
+    return(
       <Container>
          <div className={"cityRow"}>
              {
                  city.map(item =>(
-                     <Link to={`city/${item.id}`}  key={item.id}>
-                         <div>
-                             <CityBox name={item.title}/>
-                         </div>
-                     </Link>
+                            <Link to={`city/${item.id}`}key={item.id}>
+                                <div>
+                                    <CityBox name={item.title}/>
+                                </div>
+                            </Link>
                  ))
              }
          </div>
