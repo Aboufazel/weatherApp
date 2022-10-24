7e60d3c5d96c7a78ab2e40ab160f744d6c41f49b
Aboufazel <a.ab2521@gmail.com>
2022-10-24 23:17:10 +0330

    router bug fixed

--- src/components/city/cityRow.jsx
+++ src/components/city/cityRow.jsx
@@ -2,14 +2,19 @@
 import CityBox from "./cityBox";
 import Container from "../container/Container";
 import {city} from "../../getapi/Database";
-import {Link} from "react-router-dom";
+import {Link , useNavigate} from "react-router-dom";
+
 const CityRow = () => {
+    const history = useNavigate()
+    const manageHistory = ()=>{
+        history(0);
+    }
     return(
       <Container>
          <div className={"cityRow"}>
              {
                  city.map(item =>(
-                            <Link to={`city/${item.id}`}key={item.id}>
+                            <Link to={`city/${item.id}`}key={item.id} onClick={manageHistory()}>
                                 <div>
                                     <CityBox name={item.title}/>
                                 </div>
